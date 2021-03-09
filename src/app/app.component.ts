7fb13d0d2a18971d9b7e8688705e6c419e7dfe07
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-09 16:25:17 +0100

    'u'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,31 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  title = 'Bienvenue dans escape';
-  nbClassicsRooms = 4;
-  nbVirtualRooms = 5;
-  ageMin = 12;
-  ageMax = 120;
-
-  isDisabled = false;
-
-  ngOnInit(): void {
-    setTimeout(
-      () => {
-        this.title = 'Saurez-vous sortir vivant ?';
-      }, 5000
-    );  
-
-  }
-
-  lastLi() {
-    return 'Un amusement garanti';
-  }
-
-  seeMore() {
-    alert('Plus');
-  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title = 'Bienvenue dans escape';
+  nbClassicsRooms = 4;
+  nbVirtualRooms = 5;
+  ageMin = 12;
+  ageMax = 120;
+
+  isDisabled = false;
 
   ngOnInit(): void {
+    setTimeout(
+      () => {
+        this.title = 'Saurez-vous sortir vivant ?';
+      }, 5000
+    );  
+
+  }
+
+  lastLi() {
+    return 'Un amusement garanti';
+  }
+
+  seeMore() {
+    alert('Plus');
   }
 
 }
