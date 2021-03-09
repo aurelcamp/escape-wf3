99288714f26b7619ebc0a46acf3ca789d6a17756
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-09 14:11:42 +0100

    'u'

--- src/app/components/choice-nb/choice-nb.component.ts
+++ src/app/components/choice-nb/choice-nb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-choice-nb',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoiceNbComponent implements OnInit {
 
-  nbPersons = 3;
+  @Input() nbPersons = 3;
 
   constructor() { }
 
