dd845bd46ddeff31c47a82ea55c46d7ada7c5c61
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-10 13:49:50 +0100

    'u'

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+class Room {
+  title: string;
+  imageUrl: string;
+  nbMin: number;
+  nbMax: number;
+  difficulty: number;
+  description: string;
+}
+
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent implements OnInit {
+  
+  
+  room: Room = {
+    title: 'Pirate',
+    imageUrl: 'http://les-combien.com/images/rooms/pirate.jpg',
+    nbMin: 2,
+    nbMax: 4,
+    difficulty: 3,
+    description: `En tant que jeune mousse, vous devrez visiter le bateau du célèbre pirate Barbe Verte. Saurez-vous découvrir où il a caché le trésor découvert sur l'île Perdue et quitter le navire ? Mais
+    dépêchez-vous les autres pirates vous suspectent et ont décidé de vous enfermer.`
+  }
 
   constructor() { }
 
