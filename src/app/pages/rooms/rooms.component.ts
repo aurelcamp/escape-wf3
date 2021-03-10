189acf37f3b84987cecc336ef16912d217fedc44
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-10 16:31:00 +0100

    'u'

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -20,19 +20,19 @@ class Room {
 })
 export class RoomsComponent implements OnInit {
   
-  room: Room = {
-    id: 0,
-    title: 'Pirate',
-    imageUrl: 'http://les-combien.com/images/rooms/pirate.jpg',
-    imageAlt: 'Salle pirate',
-    nbMin: 2,
-    nbMax: 4,
-    difficulty: 3,
-    description: `En tant que jeune mousse, vous devrez visiter le bateau du célèbre pirate Barbe Verte. Saurez-vous découvrir où il a caché le trésor découvert sur l'île Perdue et quitter le navire ? Mais
-    dépêchez-vous les autres pirates vous suspectent et ont décidé de vous enfermer.`,
-    times: ['12h30', '17h00', '21h00'],
-    isVirtual: false,
-  }
+  // room: Room = {
+  //   id: 0,
+  //   title: 'Pirate',
+  //   imageUrl: 'http://les-combien.com/images/rooms/pirate.jpg',
+  //   imageAlt: 'Salle pirate',
+  //   nbMin: 2,
+  //   nbMax: 4,
+  //   difficulty: 3,
+  //   description: `En tant que jeune mousse, vous devrez visiter le bateau du célèbre pirate Barbe Verte. Saurez-vous découvrir où il a caché le trésor découvert sur l'île Perdue et quitter le navire ? Mais
+  //   dépêchez-vous les autres pirates vous suspectent et ont décidé de vous enfermer.`,
+  //   times: ['12h30', '17h00', '21h00'],
+  //   isVirtual: false,
+  // }
 
   rooms: Room[] = [
     {
