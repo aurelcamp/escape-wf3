2ea8922651036635e8b0aedb26de1cd1eb0984d5
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-10 15:09:42 +0100

    'u'

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -27,7 +27,7 @@ export class RoomsComponent implements OnInit {
     difficulty: 2,
     description: `En tant que jeune mousse, vous devrez visiter le bateau du célèbre pirate Barbe Verte. Saurez-vous découvrir où il a caché le trésor découvert sur l'île Perdue et quitter le navire ? Mais
     dépêchez-vous les autres pirates vous suspectent et ont décidé de vous enfermer.`,
-    isVirtual: true,
+    isVirtual: false,
   }
 
   showDescription = true;
